c9e031ff1853433820f61b8cd33d16081e8c5b49
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2023-11-23 15:41:22 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("hi i am kashif and i am learning javascript")
+
+// /--------variable--------------/
+let a = 59;  // contains 59
+console.log(a)
+ a = "kashif";
+console.log(a)
+
+// /----------rules for chosing variable name---------/
+
+// / letter, digit, underscore & $ sign allowed
+//   must began with $, _ or a letter.
+//    javascript reserved word can not be used as a variable
+
+
+
+
+
+
+
+
+
+
+
+
+
